c6aced28466c25c4999fa15839417533fd6811f1
André <ruland@outlook.de>
2021-06-23 12:49:22 +0200

    Hallo Klassen

---
+++ HalloKlassen/HalloKlassen/Fahrzeug.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace HalloKlassen
+{
+    class Fahrzeug
+    {
+        //properties / felder
+        private string _hersteller; // feld
+
+        public string GetHersteller() //getter funktion
+        {
+            return _hersteller;
+        }
+
+        internal void SetHersteller(string hersteller) //setter
+        {
+            if (hersteller != "vw")
+                _hersteller = hersteller;
+        }
+
+        private string modell; //feld
+
+        public string Modell //full-property
+        {
+            get
+            {
+                return modell;
+            }
+            internal set
+            {
+                modell = value;
+            }
+        }
+
+
+        public int PS { get; set; } //auto-property
+
+        public double KW { get { return PS * 1.6; } }
+
+        private string farbe; //backing field
+        public string Farbe
+        {
+            get { return farbe; }
+            set
+            {
+                if (value != "pink")
+                    farbe = value;
+            }
+        }
+
+        public DayOfWeek Wochentag { get; set; }
+
+        private int anzahlTüren;
+
+        public int AnzahlTüren
+        {
+            get => anzahlTüren;
+            set => anzahlTüren = value < 0 ? 0 : value;
+        }
+
+        public DateTime Baujahr { get; private set; }
+        //methoden / funktionen
+        public void MachAutoRosa()
+        {
+            Farbe = "rosa";
+        }
+
+        //ctor == nix rückgabe + name von der Klasse
+        public Fahrzeug() : this(new DateTime(1990, 1, 1)) //default konstruktor
+        {
+            Farbe = "weiss"; //default werte festlegen
+        }
+
+        public Fahrzeug(DateTime baujahr)
+        {
+            Baujahr = baujahr;
+        }
+        public Fahrzeug(string hersteller, string modell, string farbe, int ps)
+        {
+            this._hersteller = hersteller;
+            Modell = modell;
+            Farbe = farbe;
+            PS = ps;
+        }
+
+        ~Fahrzeug() //destruktor
+        {
+                
+        }
+    }
+
+}
---
+++ HalloKlassen/HalloKlassen/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace HalloKlassen
+{
+    class Program
+    {
+        static int zahl = 23;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(zahl);
+            Verdoppeln(zahl);
+            Console.WriteLine(zahl);
+
+            { //scope
+                var ran = new Random();
+
+            }
+
+            Fahrzeug meinFahrzeug = null; //deklaration
+            meinFahrzeug = new Fahrzeug(); //instanzierung
+            meinFahrzeug.Modell = "B45";
+            meinFahrzeug.Farbe = "gelb";
+            meinFahrzeug.PS = 128;
+            meinFahrzeug.SetHersteller("Baudi");
+
+            Fahrzeug fahrzeugMitTollenCtor = new Fahrzeug("Bord", "OLO", "blau", 100);
+
+            Fahrzeug anderesFahrzeug = new Fahrzeug(new DateTime(2000, 1, 1)); //deklaration und instanzierung in einer Zeile
+                                                                               //var anderesFahrzeug2 = new Fahrzeug(); //das gleiche in kürzer
+                                                                               //anderesFahrzeug.hersteller = "Besla";
+            anderesFahrzeug.SetHersteller("Besla");
+            anderesFahrzeug.Modell = "S1";
+            //anderesFahrzeug.Farbe = "rot";
+            anderesFahrzeug.PS = 348;
+            anderesFahrzeug.Wochentag = DayOfWeek.Monday;
+
+
+            ZeigeFahrzeug(meinFahrzeug);
+            meinFahrzeug.MachAutoRosa();
+            ZeigeFahrzeug(meinFahrzeug);
+
+            ZeigeFahrzeug(anderesFahrzeug);
+        }
+
+        static void Verdoppeln(int einezahl)
+        {
+            zahl = 12;
+            einezahl = einezahl * 2;
+        }
+
+
+        static void ZeigeFahrzeug(HalloKlassen.Fahrzeug einFahrzeug)
+        {
+            einFahrzeug.Modell += "LALALAA";
+            Console.WriteLine($"{einFahrzeug.GetHersteller() } {einFahrzeug.Modell} {einFahrzeug.Farbe} {einFahrzeug.PS}");
+
+        }
+    }
+
+}
